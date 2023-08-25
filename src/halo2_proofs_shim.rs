b436dd4eccbfee385de8800554c917c25354e264
Ayush Shukla <shuklaayush247@gmail.com>
2023-08-26 01:06:56 +0200

    fix: typos

--- src/halo2_proofs_shim.rs
+++ src/halo2_proofs_shim.rs
@@ -3,14 +3,14 @@
     not(feature = "pse-backend"),
     not(feature = "zcash-backend")
 ))]
-compile_error!("one backend must be enabled");
+compile_error!("no backend enabled");
 
 #[cfg(any(
     all(feature = "axiom-backend", feature = "pse-backend",),
     all(feature = "axiom-backend", feature = "zcash-backend"),
     all(feature = "pse-backend", feature = "zcash-backend"),
 ))]
-compile_error!("only one backend cannot be enabled at a time");
+compile_error!("only one backend can be enabled at a time");
 
 #[cfg(feature = "axiom-backend")]
 pub use halo2_proofs_axiom::*;
